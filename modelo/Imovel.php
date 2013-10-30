162fa1317921ea28c1fefc062811bcfb928b8d38
root <root@adonias-HP-Mini-210-2100>
2013-10-30 19:33:50 -0300

    Adaptação do Modelo para receber DAO genérico.

---
+++ modelo/Imovel.php
@@ -0,0 +1,152 @@
+<?php
+
+class Imovel {
+
+    private $id;
+    private $valor;
+    private $finalidade;
+    private $quarto;
+
+    /*   private $suite;
+      private $area;
+      private $garagem;
+      private $banheiro;
+      private $topicoImovel;
+      private $descricaoImovel;
+      private $endereco;
+      private $buscaAvancada;
+      private $imagem; */
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getValor() {
+        return $this->valor;
+    }
+
+    public function getFinalidade() {
+        return $this->finalidade;
+    }
+
+    public function getQuarto() {
+        return $this->quarto;
+    }
+
+/*
+
+      public function getSuite() {
+      return $this->suite;
+      }
+
+      public function getArea() {
+      return $this->area;
+      }
+
+      public function getGaragem() {
+      return $this->garagem;
+      }
+
+      public function getBanheiro() {
+      return $this->banheiro;
+      }
+
+      public function getTopicoImovel() {
+      return $this->topicoImovel;
+      }
+
+      public function getDescricaoImovel() {
+      return $this->descricaoImovel;
+      }
+
+      public function getEndereco() {
+      return $this->endereco;
+      }
+
+      public function getBuscaAvancada() {
+      return $this->buscaAvancada;
+      }
+
+      public function getImagem() {
+      return $this->imagem;
+      }
+     */
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function setValor($valor) {
+        $this->valor = $valor;
+    }
+
+    public function setFinalidade($finalidade) {
+        $this->finalidade = $finalidade;
+    }
+
+    public function setQuarto($quarto) {
+        $this->quarto = $quarto;
+    }
+
+    /*
+      public function setSuite($suite) {
+      $this->suite = $suite;
+      }
+
+      public function setArea($area) {
+      $this->area = $area;
+      }
+
+      public function setGaragem($garagem) {
+      $this->garagem = $garagem;
+      }
+
+      public function setBanheiro($banheiro) {
+      $this->banheiro = $banheiro;
+      }
+
+      public function setTopicoImovel($topicoImovel) {
+      $this->topicoImovel = $topicoImovel;
+      }
+
+      public function setDescricaoImovel($descricaoImovel) {
+      $this->descricaoImovel = $descricaoImovel;
+      }
+
+      public function setEndereco($endereco) {
+      $this->endereco = $endereco;
+      }
+
+      public function setBuscaAvancada($buscaAvancada) {
+      $this->buscaAvancada = $buscaAvancada;
+      }
+
+      public function setImagem($imagem) {
+      $this->imagem = $imagem;
+      } */
+
+    function cadastrar($parametros) {
+
+        $imovel = new Imovel();
+
+        $imovel->setValor($parametros['txtValor']);
+        $imovel->setFinalidade($parametros['sltFinalidade']);
+        $imovel->setQuarto($parametros['sltQuarto']);
+
+        return $imovel;
+    }
+    
+        function editar($parametros) {
+
+        $imovel = new Imovel();
+
+        $imovel->setId($parametros['hdnId']);
+        $imovel->setValor($parametros['txtValor']);
+        $imovel->setFinalidade($parametros['sltFinalidade']);
+        $imovel->setQuarto($parametros['sltQuarto']);
+
+        return $imovel;
+    }
+}
+
+
