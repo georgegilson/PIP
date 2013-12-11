6e02e9c76f774e840f9a2df4dada47ea5a3601da
root <root@adonias-HP-Mini-210-2100>
2013-12-11 00:42:22 -0300

    Alterar Usuário

--- modelo/Usuario.php
+++ modelo/Usuario.php
@@ -14,13 +14,31 @@ class Usuario {
     private $email;
     private $idendereco;
     protected $endereco;
+    protected $telefone;
+    protected $empresa;
     
-    public function setEndereco($endereco) {
-        $this->endereco = $endereco;
+    public function setEmpresa($empresa) {
+        $this->empresa = $empresa;
+    }
+    
+    public function getEmpresa() {
+        return $this->empresa;
+    }
+    
+    public function setTelefone($telefone) {
+        $this->telefone = $telefone;
+    }
+    
+    public function getTelefone() {
+        return $this->telefone;
     }
     
     public function getEndereco() {
         return $this->endereco;
+    }
+
+    public function setEndereco($endereco) {
+        $this->endereco = $endereco;
     }
 
     public function getEmail() {
@@ -116,13 +134,6 @@ class Usuario {
         $usuario->setTipousuario($parametros['sltTipoUsuario']);
         $usuario->setNome($parametros['txtNome']);
         $usuario->setLogin($parametros['txtLogin']);
-//        $td = mcrypt_module_open('rijndael-256', '', 'ofb', '');       
-//        $iv = mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_DEV_RANDOM);
-//        $ks = mcrypt_enc_get_key_size($td);       
-//        $key = substr(md5('very secret key'), 0, $ks);     
-//        mcrypt_generic_init($td, $key, $iv);
-//        $encrypted = mcrypt_generic($td, $parametros['txtSenha']);    
-//        mcrypt_generic_deinit($td);
         $usuario->setSenha($parametros['txtSenha']);
         if ($usuario->getTipousuario() == "fisica") {
             $usuario->setCpfcnpj($parametros['txtCpf']);
@@ -134,6 +145,25 @@ class Usuario {
         $usuario->setDatahoracadastro(date('d/m/Y H:i:s'));
         $usuario->setDatahoraalteracao("");
         $usuario->setIdendereco($idendereco);
+        return $usuario;
+    }
+    function editar($parametros){      
+        $usuario = new Usuario();
+        $usuario->setId($parametros['hdnId']);
+        $usuario->setTipousuario($parametros['sltTipoUsuario']);
+        $usuario->setNome($parametros['txtNome']);
+        $usuario->setLogin(NULL);
+        $usuario->setSenha(NULL);
+        if ($usuario->getTipousuario() == "fisica") {
+            $usuario->setCpfcnpj($parametros['txtCpf']);
+        } else {
+            $usuario->setCpfcnpj($parametros['txtCnpj']);
+        }
+        $usuario->setEmail($parametros['txtEmail']);
+        $usuario->setStatus("A");
+        $usuario->setDatahoracadastro(NULL);
+        $usuario->setDatahoraalteracao(date('d/m/Y H:i:s'));
+        $usuario->setIdendereco(NULL);
         return $usuario;
     }
 
