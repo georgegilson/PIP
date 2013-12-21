31716ecb4b73eccb616c96ed4406cf497124effa
root <root@adonias-HP-Mini-210-2100>
2013-12-21 08:19:28 -0300

    SPRINT 20/12/2013

--- configuracao/Conexao.php
+++ configuracao/Conexao.php
@@ -11,7 +11,7 @@ class Conexao {
     public static function getInstance() {
 
         if (!isset(self::$instance)) {
-            self::$instance = new PDO('mysql:host=localhost;dbname=pip', 'root', '', 
+            self::$instance = new PDO('mysql:host=localhost;dbname=pip', 'root', 'admin', 
                     array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
             self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$instance->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
